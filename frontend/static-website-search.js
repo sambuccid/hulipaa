cfc63dc19a3c1f26f92b2abb27d7402af62b85db
sambuccid <sambuccid@gmail.com>
2022-10-16 14:04:20 +0000

    refactor to make creation of html more readable(and extendable)

--- frontend/static-website-search.js
+++ frontend/static-website-search.js
@@ -1,4 +1,46 @@
 window.addEventListener('DOMContentLoaded', () => {
+    const EL={
+        textarea: function(){
+            return document.createElement("input");
+        },
+        button: function(options){
+            const { els } = options;
+            options.els = undefined;
+            const button = document.createElement("button");
+
+            els.forEach((el)=>{
+                button.appendChild(el);
+            })
+
+            return button;
+        },
+        img: function(options){
+            const img = document.createElement("img");
+            for(const prop in options){
+                img[prop] = options[prop];
+            }
+            return img;
+        },
+        form: function(options){
+            const { els } = options;
+            options.els = undefined;
+            const form = document.createElement("form");
+
+            els.forEach((el)=>{
+                form.appendChild(el);
+            })
+
+            for(const prop in options){
+                form[prop] = options[prop];
+            }
+            return form;
+        }
+        // },
+        // _createElement: function(name, options){
+
+        // }
+    }
+
     const searchDivs = document.getElementsByClassName("sws_searchbar")
     
     for(let i=0; i<searchDivs.length; i++){
@@ -23,32 +65,29 @@ window.addEventListener('DOMContentLoaded', () => {
 
 
     function addSearchBarElements(searchDiv){
-        const textarea = document.createElement("input");
-        
-        const button = document.createElement("button");
-
-        const searchImg = document.createElement("img")
-        searchImg.innerText = "Search"
-        searchImg.src = "../frontend/images/glass_magnifier_icon.svg"
-        searchImg.width = 30
-        searchImg.height = 30
-
-        button.appendChild(searchImg)
-
-        const form = document.createElement("form")
-        form.appendChild(textarea)
-        form.appendChild(button)
-
-        form.onsubmit = function(event){
-            event.preventDefault()
-            onClickSearch(searchDiv, textarea)
-            return false
-        }
-        
+        const textarea = EL.textarea()
+        const form = EL.form({els:[
+                textarea,
+                EL.button({els:[
+                    EL.img({
+                        innerText: "Search",
+                        src: "../frontend/images/glass_magnifier_icon.svg",
+                        width: 30,
+                        height: 30
+                    })
+                ]})
+            ],
+            onsubmit: function(event){
+                event.preventDefault()
+                onClickSearch(searchDiv, textarea)
+                return false
+            }
+        });
         searchDiv.appendChild(form)
     }
 
 
+                    
     // function addSearchBarElements(searchDiv){
     //     const nodes = searchBarTemplate({
     //         onButtonClick: onClickSearch.bind(null, searchDiv, textarea)
