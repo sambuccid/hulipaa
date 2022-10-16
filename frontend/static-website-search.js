dcb8a684f6fd563865803911a8a51e8d50145012
sambuccid <sambuccid@gmail.com>
2022-10-16 13:33:24 +0000

    possible refactor(not completed) to use string literals for HTML

--- frontend/static-website-search.js
+++ frontend/static-website-search.js
@@ -38,4 +38,38 @@ window.addEventListener('DOMContentLoaded', () => {
         searchDiv.appendChild(textarea)
         searchDiv.appendChild(button)
     }
+
+
+    // function addSearchBarElements(searchDiv){
+    //     const nodes = searchBarTemplate({
+    //         onButtonClick: onClickSearch.bind(null, searchDiv, textarea)
+    //     });
+    //     nodes.forEach((node)=>{
+    //         searchDiv.appendChild(node)
+    //     });
+    // }
+    // function searchBarTemplate({onButtonClick}){
+    //     let template = `
+    //         <input></input>
+    //         <button>
+    //             <img src="../frontend/images/glass_magnifier_icon.svg" width="30" height="30">Search</img>
+    //         </button>
+    //     `;
+    //     const nodes = parseHtml(template);
+    //     const button = nodes[1];
+    //     button.onclick = onButtonClick;
+    //     return nodes;
+    // }
+    // function parseHtml(html){
+    //     const template = document.createElement("template");
+    //     template.innerHTML = html;
+    //     const children = template.content.children;
+    //     if(children.length<=0){
+    //         return null;
+    //     } else if(children.length==1){
+    //         return children[0];
+    //     } else {
+    //         return children;
+    //     }
+    // }
 });
