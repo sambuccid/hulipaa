b5aa0942d819c75d95da4b9d219274e1216a211e
sambuccid@gmail.com <sambuccid@gmail.com>
2022-12-18 13:22:05 +0000

    Correct path to load results

--- frontend/service.js
+++ frontend/service.js
@@ -2,11 +2,12 @@ import { get } from './network.js'
 
 export async function search(query) {
     //TODO always reference root url(instead of relative path)
-    const result = await get("search/" + query);
+    const result = await get("/search/" + query + ".json");
     // const result = await get("../.output/big-index.json");
 
     if (!result.ok) {
         throw "Error!!!!"
     }
     return await result.json()
+    //return {results:[{title: "aaaaaaa"}]}
 }
