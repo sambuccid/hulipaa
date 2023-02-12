b9bba5709d26c9bf096b0d10f93a6b798dba4e65
sambuccid <sambuccid@gmail.com>
2023-02-12 20:08:48 +0000

    avoiding cutting text mid-word when formatting

--- frontend/general.js
+++ frontend/general.js
@@ -24,7 +24,7 @@ async function onResultClick(resultPath,resultDiv,searchedWord) {
     ResultsUI.populateWith({ resultDiv,text: formattedText })
 }
 const N_CHARS_CUT_TEXT = 20
-function formatTextForResult(text,searchedWord) {
+function formatTextForResult(text,searchedWord) { //TODO needs refactoring to simplify
     //find line containig result
     const separateLines = text.split(/\r?\n|\r|\n/g);
     const lineWithSearchedWord = separateLines.find(line => line.includes(searchedWord));
@@ -34,10 +34,18 @@ function formatTextForResult(text,searchedWord) {
     }
     let formattedText = lineWithSearchedWord
 
-    //extract text right before and after searchedWord
+    //extract text just right before and right after searchedWord
     let searchedWordIdx = formattedText.indexOf(searchedWord)
     if (searchedWordIdx > N_CHARS_CUT_TEXT) {
-        formattedText = formattedText.substring(searchedWordIdx - N_CHARS_CUT_TEXT)
+        let idxWhereToCut = searchedWordIdx - N_CHARS_CUT_TEXT
+        //try to avoid half-words in formatted text, when cutting text it should not cut words in half
+        if (isIndexMidWord(idxWhereToCut,formattedText)) {
+            //find first index not mid word
+            while (idxWhereToCut < formattedText.length && isIndexMidWord(idxWhereToCut,formattedText)) {
+                idxWhereToCut++
+            }
+        }
+        formattedText = formattedText.substring(idxWhereToCut)
         formattedText = '...' + formattedText
     }
 
@@ -45,9 +53,45 @@ function formatTextForResult(text,searchedWord) {
     const searchedWordRightIdx = searchedWordIdx + searchedWord.length
     const nCharsToEndOfLine = formattedText.length - searchedWordRightIdx
     if (nCharsToEndOfLine > N_CHARS_CUT_TEXT) {
-        formattedText = formattedText.substring(0,searchedWordRightIdx + N_CHARS_CUT_TEXT)
+        let idxWhereToCut = searchedWordRightIdx + N_CHARS_CUT_TEXT
+        //try to avoid half-words in formatted text, when cutting text it should not cut words in half
+        if (isIndexMidWord(idxWhereToCut,formattedText)) {
+            //find first index not mid word
+            while (idxWhereToCut != 0 && isIndexMidWord(idxWhereToCut,formattedText)) {
+                idxWhereToCut--
+            }
+        }
+        formattedText = formattedText.substring(0,idxWhereToCut)
         formattedText = formattedText + '...'
     }
-    //check if is cutting a word in half, if it is exclude the word(3rd and 4th parameter of parametrised test)
+
+
+    function isIndexMidWord(idx,text) {
+        if (!isCharPartOfWord(text[idx])) {
+            return false
+        }
+        if (idx > 0 && isCharPartOfWord(text[idx - 1])) {
+            return true
+        }
+        if (idx > text.length && isCharPartOfWord(text[idx + 1])) {
+            return true
+        }
+
+        return false;
+    }
+
+    function isCharPartOfWord(char) {
+        if (char.length > 1) throw "passed input contains more than 1 character"
+
+        if (char === '_' || char === '-' || char === "'") {
+            return true;
+        }
+        if (new RegExp('[A-Za-zÀ-ÖØ-öø-ÿ]').test(char)) {
+            return true
+        }
+
+        return false
+    }
+
     return formattedText
 }
